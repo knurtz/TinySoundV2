2448b7212df8a32f8d37228b5fc79ee691dc6c51
Johannes Herold <johannes.herold2@tu-dresden.de>
2023-04-03 14:30:55 +0200

    New component arrangement vor v2.1

--- Code/TinySound/TinySound.c
+++ Code/TinySound/TinySound.c
@@ -12,18 +12,45 @@
 #include "TS_fat.h"
 
 
+#define LED_PIN         28
 uint32_t next_blink_ms = 0;
-uint16_t blink_interval_ms = 200;
-uint16_t blink_interval_fast_ms = 100;
+uint16_t blink_interval_ms = 500;
+uint16_t blink_interval_fast_ms = 200;
 
 void blink(void)
 {
     if (time_us_32() / 1000 > next_blink_ms)
     {
         next_blink_ms += Audio_IsPlaying() ? blink_interval_fast_ms : blink_interval_ms;
-        gpio_put(28, !gpio_get(28));
+        gpio_put(LED_PIN, !gpio_get(LED_PIN));
     }
 }
+
+
+#define TRIGGER_PIN     5
+#define TRIGGER_DELAY 10000      // us
+uint32_t last_trigger = 0;      // us
+uint8_t trigger_counter = 0;
+
+void __isr __time_critical_func(trigger_callback)(uint gpio, uint32_t events)
+{
+    trigger_counter++;
+    last_trigger = time_us_32();
+}
+
+
+char* tracklist[] =
+    {
+        "01.wav",
+        "02.wav",
+        "03.wav",
+        "04.wav",
+        "05.wav",
+        "06.wav",
+        "07.wav",
+        "08.wav",
+        "09.wav"
+    };
 
 
 int main()
@@ -35,17 +62,17 @@ int main()
     Flash_Init();
     Audio_Init();
 
-    // Init LED output (GPIO3)
-    gpio_init(28);
-    gpio_set_dir(28, GPIO_OUT);
+    // Init LED output
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    // Init play trigger input (GPIO2)
-    gpio_init(18);
-    gpio_set_dir(18, GPIO_IN);
-    gpio_pull_up(18);
+    // Init play trigger input
+    gpio_init(TRIGGER_PIN);
+    gpio_set_dir(TRIGGER_PIN, GPIO_IN);
+    gpio_disable_pulls(TRIGGER_PIN);
+    gpio_set_irq_enabled_with_callback(TRIGGER_PIN, GPIO_IRQ_EDGE_FALL, true, &trigger_callback);
 
     FAT_Init();
-    //Audio_Play("preuss~1.wav");
 
     while (true)
     {
@@ -53,10 +80,15 @@ int main()
         Shell_CheckCommand();
         Flash_WriteCycle(false);
         Audio_CheckBuffer();
-
-        if (!gpio_get(18) && !Audio_IsPlaying())
+        
+        if (trigger_counter && time_us_32() > last_trigger + TRIGGER_DELAY)
         {
-            Audio_Play("moep.wav");
+            xprintf("Triggered %d times\n", trigger_counter);
+
+            if (trigger_counter < count_of(tracklist));
+                Audio_Play(tracklist[trigger_counter]);
+
+            trigger_counter = 0;
         }
 
         blink();
